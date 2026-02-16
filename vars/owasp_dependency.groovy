3822cd67572b18c2fd77864b0043eb89b90729f9
mujeebalia123 <130073399+mujeebalia123@users.noreply.github.com>
2026-02-16 15:32:21 +0530

    Update owasp_dependency.groovy

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -4,7 +4,7 @@
 #}
 
 def call(){
-  dependencyCheck additionalArguments: '--scan ./ --format XML', 
+  dependencyCheck additionalArguments: '--scan . --format XML --out .',
                   odcInstallation: 'OWASP'
 
   dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
